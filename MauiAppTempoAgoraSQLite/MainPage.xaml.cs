9e728b4d3416d0ec805ee4c8d65584d3c38ae43c
231471 <231471@CPD02-M07>
2025-05-19 10:53:00 -0300

    19/05

--- MauiAppTempoAgoraSQLite/MainPage.xaml.cs
+++ MauiAppTempoAgoraSQLite/MainPage.xaml.cs
@@ -1,24 +1,62 @@
-﻿namespace MauiAppTempoAgoraSQLite
+﻿using MauiAppTempoAgoraSQLite.Models;
+using MauiAppTempoAgoraSQLite.Services;
+
+namespace MauiAppTempoAgoraSQLite
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
 
-        public MainPage()
+        public partial class MainPage : ContentPage
         {
-            InitializeComponent();
-        }
+            int count = 0;
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+            public MainPage()
+            {
+                InitializeComponent();
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            private async void Button_Clicked(object sender, EventArgs e)
+            {
+                try
+                {
+                    if (!string.IsNullOrEmpty(txt_cidade.Text))
+                    {
+                        Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        if (t != null)
+                        {
+                            string dados_previsao = "";
+
+                            dados_previsao = $"Latitude: {t.lat} \n" +
+                                             $"Longitude: {t.lon} \n" +
+                                             $"Nascer do Sol: {t.sunrise} \n" +
+                                             $"Por do Sol: {t.sunset} \n" +
+                                             $"Temp Máx: {t.temp_max} \n" +
+                                             $"Temp Min: {t.temp_min} \n";
+
+                            lbl_res.Text = dados_previsao;
+
+                            await App.Db.Insert(t);
+
+                        }
+                        else
+                        {
+                            lbl_res.Text = "Sem dados de Previsão";
+                        } // Fecha if t=null
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Preencha a cidade.";
+                    } // fecha if string is null or empty
+
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Ops", ex.Message, "OK");
+                }
+            }
         }
     }
 
---
+++ MauiAppTempoAgoraSQLite/Models/tempo.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace MauiAppTempoAgoraSQLite.Models
+{
+    public class Tempo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public double? lon { get; set; }
+        public double? lat { get; set; }
+        public double? temp_min { get; set; }
+        public double? temp_max { get; set; }
+        public int? visibility { get; set; }
+        public double? speed { get; set; }
+        public string? main { get; set; }
+        public string? description { get; set; }
+        public string? sunrise { get; set; }
+        public string? sunset { get; set; }
+    }
+}
